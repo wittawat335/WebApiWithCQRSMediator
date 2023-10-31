f3836e3e0f38db00ed7807a10224760ab48b1e14
Wittawat Fuangfu <wittawat335@gmail.com>
2023-10-31 12:04:04 +0700

    add all Layer

--- WebApiWithCQRSMediator.Core/ConfigureServices.cs
+++ WebApiWithCQRSMediator.Core/ConfigureServices.cs
@@ -1,8 +1,20 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
 namespace WebApiWithCQRSMediator.Core
 {
     public static class ConfigureServices
     {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(ctg =>
+            {
+                ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
+            return services;
+        }
     }
 }
---
+++ WebApiWithCQRSMediator.Infra/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiWithCQRSMediator.Infra
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfraServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
